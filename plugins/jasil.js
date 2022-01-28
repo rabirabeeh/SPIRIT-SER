4a15ccc6ce93c817d15a871aaddcd8571db8b7ce
Spirit ser <96570722+SPlRlT-YT@users.noreply.github.com>
2022-01-28 21:30:48 +0530

    Update jasil.js

--- plugins/jasil.js
+++ plugins/jasil.js
@@ -7,21 +7,21 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.imgur.com/CtaexU3.jpeg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://i.imgur.com/Mbaz2g9.jpg", { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Bᴏᴛ Nᴀᴍᴇ 🇦 🇲 🇷 🇺   🇸 🇪 🇷  🇧 🇴 🇹 *
 
-*Cʀᴇᴀᴛᴇʀ number : wa.me/917025631103?text=Hi%20sᴘɪʀɪᴛ%20bro.%20*
+*Cʀᴇᴀᴛᴇʀ number : wa.me/12098020068?text=Hi%20sᴘɪʀɪᴛ%20bro.%20
 
 
- *Sᴘɪʀɪᴛ sᴇʀ Cʜᴀɴᴀʟ : https://youtube.com/channel/UCBv1Z1zkOmdGeWU2QRdYBnA*
+ *Sᴘɪʀɪᴛ sᴇʀ Cʜᴀɴᴀʟ : https://youtube.com/channel/UCBv1Z1zkOmdGeWU2QRdYBnA
      
  
- *Sᴘɪʀɪᴛ sᴇʀ Bᴏᴛ Gʀᴏᴜᴘ: https://chat.whatsapp.com/DnHJu25Ccss7zn72nPhL8z*
+ *Sᴘɪʀɪᴛ sᴇʀ Bᴏᴛ Gʀᴏᴜᴘ: https://chat.whatsapp.com/F3adhxvYpd3KzVptzDxhC4
 
- *Gɪᴛ Lɪɴᴋ : https://github.com/AMRUSIR/AMRU-SER*
+ *Gɪᴛ Lɪɴᴋ : https://github.com/SPlRlT-YT/spirit-ser
 
- *Sᴘɪʀɪᴛ-sᴇʀ-ʙᴏᴛꫂ⁩..♡︎*
+ Sᴘɪʀɪᴛ-sᴇʀ-ʙᴏᴛꫂ⁩..♡︎
 `}) 
 
 }));
