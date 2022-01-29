d09d98568764e3d86bcf4c4df01c68d929ac9dd1
Spirit ser <96570722+SPlRlT-YT@users.noreply.github.com>
2022-01-29 10:12:55 +0530

    Update jasilser.js

--- plugins/jasilser.js
+++ plugins/jasilser.js
@@ -21,7 +21,7 @@ Neotro.addCommand({pattern: 'help', fromMe: false, dontAddCommandList: true}, (a
 // send a list message!
     const rows = [
         {title: 'Sᴘɪʀɪᴛ Bᴏᴛ Oᴡɴᴇʀs Nᴜᴍʙᴇʀ', description: "⚙Bᴏᴛ Oᴡɴᴇʀ Nᴜᴍᴇʀ\n\n\n*◁ === Tɪs Oᴡɴᴇʀs sᴘɪʀɪᴛ Sᴇʀ Nᴀɴʙᴜɴ Aɴᴅ Cʜᴜɴᴋs === ▷*\n\n🎭 *sᴘɪʀɪᴛ Bᴏᴛ Bʏ sᴘɪʀɪᴛ sᴇʀ *\n 🔮 http://wa.me/12098020068.\n\n🎭\n\n ", rowId:" rowid1"},
-        {title: 'Gɪᴛ', description: "Aʙᴜ Sᴇʀ Bᴏᴛ Mᴀᴋɪɴɢ Lɪɴᴋ.\n\n\n*◁ === 🥰🤗 Gɪᴛ Lɪɴᴋ === ▷*\n\nhttps://github.com/SPlRlT-YT/spirit-ser\n\n ", rowId:"rowid2"},
+        {title: 'Gɪᴛ', description: "Sᴘɪʀɪᴛ Sᴇʀ Bᴏᴛ Mᴀᴋɪɴɢ Lɪɴᴋ.\n\n\n*◁ === 🥰🤗 Gɪᴛ Lɪɴᴋ === ▷*\n\nhttps://github.com/SPlRlT-YT/spirit-ser\n\n ", rowId:"rowid2"},
         {title: 'Sᴘɪʀɪᴛ Bᴏᴛ Oғғɪᴄᴀʟ Gʀᴏᴜᴘ Bʏ sᴘɪʀɪᴛ', description: "Gʀᴏᴜᴘ Lɪɴᴋ .\n\n\nhttps://chat.whatsapp.com/Bq0eHs3UpGJ2BKIHOmy7mk\n\n ", rowId:" rowid5"},
         {title: 'Sᴘɪʀɪᴛ Sᴇʀ Cʜᴀɴᴀʟ Lɪɴᴋ ', description: "sᴘɪʀɪᴛ sᴇʀ Cʜᴀɴᴀʟ\n\n\n*sᴘɪʀɪᴛ Sᴇʀ Cʜᴀɴᴀʟ Sᴜʙsᴄʀᴀɪʙᴇ*\n\n*https://youtube.com/channel/UCBv1Z1zkOmdGeWU2QRdYBnA* ", rowId:" rowid6"},
         {title: 'Bᴏᴛ Gʀᴏᴜᴘ Dᴇsᴄʀɪᴘᴛɪᴏɴ', description: "Nɪʏᴀᴍᴀᴍ Iɴɪ Eʟʟᴀᴠᴀʀᴋᴋᴜᴍ Bᴀᴅʜᴀᴋᴀ Mᴀɴ \n\n\n☃️ᴏɴɴᴜᴍ ɪʟʟᴀ ɢʀᴏᴜᴘ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ😁 \n\n  ", rowId:" rowid7"}
